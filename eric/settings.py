76dadf60a7732613f964aa866aa60532299b3b35
Theogene-dotcom <64300797+Theogene-dotcom@users.noreply.github.com>
2024-11-22 10:36:01 -0800

    Update settings.py

--- eric/settings.py
+++ eric/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3&8v^%v-efws#+@7=dlq3^pm6g651pfug#pdvw3255nc0$44dg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['erictech-production.up.railway.app']
 
 
 # Application definition

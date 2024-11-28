8f61d21f7edbe46d02f23b25acbda85f25ddf639
Theogene-dotcom <64300797+Theogene-dotcom@users.noreply.github.com>
2024-11-28 04:12:57 -0800

    cloudinary 2

--- ericapp/models.py
+++ ericapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django_ckeditor_5.fields import CKEditor5Field
+from cloudinary.models import CloudinaryField
 
 class Contact(models.Model):
     name = models.CharField(max_length=100)
@@ -13,11 +14,12 @@ class Contact(models.Model):
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     content = CKEditor5Field(config_name='extends', null=True, blank=True)
-    image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
+    image = CloudinaryField('image', null=True, blank=True)  # Use CloudinaryField for image storage
     published_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
 
 class Comment(models.Model):
     blog_post = models.ForeignKey(BlogPost, related_name='comments', on_delete=models.CASCADE)

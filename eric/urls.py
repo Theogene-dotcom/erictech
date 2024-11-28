6737200648b282e2595a2ff56a3af69a6913b5cf
Theogene-dotcom <64300797+Theogene-dotcom@users.noreply.github.com>
2024-11-28 04:00:51 -0800

    cloudinary video

--- eric/urls.py
+++ eric/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('', include('ericapp.urls')),
     path('ckeditor5/', include('django_ckeditor_5.urls')),
 ]
